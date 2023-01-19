a84c295b14535779467a6576adccfdf5be7affc4
HONGKAI123 <huanghongkai98@GMAIL.COM>
2023-01-19 01:15:51 -0800

    update

---
+++ DOM/Drum Kit Starting Files/index.js
@@ -0,0 +1,18 @@
+for( var i = 0; i < document.querySelectorAll(".drum").length;i++)
+{
+   document.querySelectorAll('.drum')[i].addEventListener("click", function(){
+    alert('Hello')
+   })
+}
+
+// document.querySelector('button').addEventListener('click', handclick)  给所有第一个按钮加alert
+
+// function handclick(){
+   
+//   alert('hey fuck you usj')
+// }
+
+// const click = document.querySelector('button')
+// click.addEventListener('click', function (){
+//    alert('hey fuck you usj')
+//   }) 
