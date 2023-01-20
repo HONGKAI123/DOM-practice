05d987493943e2a4a14596c2e17026ebcb835b0c
HONGKAI123 <huanghongkai98@GMAIL.COM>
2023-01-20 00:51:06 -0800

    update

--- DOM/Drum Kit Starting Files/index.js
+++ DOM/Drum Kit Starting Files/index.js
@@ -1,7 +1,8 @@
 for( var i = 0; i < document.querySelectorAll(".drum").length;i++)
 {
    document.querySelectorAll('.drum')[i].addEventListener("click", function(){
-    alert('Hello')
+      var audio = new Audio('./sounds/crash.mp3');
+      audio.play();
    })
 }
 
