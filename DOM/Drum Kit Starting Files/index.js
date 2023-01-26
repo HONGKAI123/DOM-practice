f4082a22de62c9b97dd76ce94e2f34fb2cd3df6b
HONGKAI123 <huanghongkai98@GMAIL.COM>
2023-01-26 02:08:51 -0800

    1

--- DOM/Drum Kit Starting Files/index.js
+++ DOM/Drum Kit Starting Files/index.js
@@ -1,11 +1,26 @@
 for( var i = 0; i < document.querySelectorAll(".drum").length;i++)
 {
    document.querySelectorAll('.drum')[i].addEventListener("click", function(){
-     console.log(this.style.color = 'white')    //this 可以用来检测哪个按键被按了 this.innerHTML可以显示HTML字符
-   })  //未完成： 使用不同的switch statement来完成每个按键不同的音乐
+     //this 可以用来检测哪个按键被按了 this.innerHTML可以显示HTML字符
+     var sound = this.innerHTML;
+     switch(sound){
+        case "a" :
+          var audio = new Audio('./sounds/crash.mp3');
+          audio.play();
+          break;
+        case "w":
+          var audio = new Audio('./sounds/kick-bass.mp3');
+          audio.play();
+          break;
+     }
+      
+   })  //已完成： 使用不同的switch statement来完成每个按键不同的音乐
 }
+document.addEventListener("keypress",function(e){  //e在这里会打印出被按的按键 
 
-// var audio = new Audio('./sounds/crash.mp3');
+})  //给所有该页面上的东西加event listener
+
+// var audio = new Audio('./sounds/crash.mp3');  //添加音乐
 //       audio.play();
 
 
